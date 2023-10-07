cca1503a4bc942197f52523c2b8ac74c97ee57a9
Duong Ba Nhat <banhatduong@gmailcom>
2023-10-07 14:34:37 +0700

    Linking Between Pages

--- 01-starting-project/pages/news/index.js
+++ 01-starting-project/pages/news/index.js
@@ -1,3 +1,18 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 export default function NewsPage(params) {
-    return <h1>News page</h1>
-};
+  return (
+    <Fragment>
+      <h1>News page</h1>
+      <ul>
+        <li>
+          <Link href={'/news/football'}>News football</Link>
+        </li>
+        <li>
+          <Link href={'/news/sport'}>News Sport</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+}
