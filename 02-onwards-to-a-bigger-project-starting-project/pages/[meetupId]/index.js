f2a919c4102ff270b3e0e06f77339a10a09f9e6f
Duong Ba Nhat <banhatduong@gmailcom>
2023-10-08 14:42:02 +0700

    Adding Custom Components & CSS Modules

---
+++ 02-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      title="first meetup"
+      image="https://cdn.tgdd.vn/Products/Images/44/301634/hp-15s-fq2716tu-i3-7c0x3pa-thumb-600x600.jpg"
+      address="some address 5, 12345, city"
+      description="Some descriptions"
+    />
+  );
+}
+
+export default MeetupDetails;
