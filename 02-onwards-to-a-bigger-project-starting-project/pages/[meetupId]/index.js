7b3777cf603baf289dc47bb2268aa7dace6f07e9
Duong Ba Nhat <banhatduong@gmailcom>
2023-10-09 20:31:31 +0700

    Working with Params for SSG Data Fetching, Preparing Paths with "getStaticPaths" & Working With Fallback Pages

--- 02-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
+++ 02-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
@@ -1,6 +1,6 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
       title="first meetup"
@@ -11,4 +11,38 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const params = context.params;
+  console.log(params.meetupId);
+
+  return {
+    props: {
+      meetup: {
+        title: 'first meetup',
+        image:
+          'https://cdn.tgdd.vn/Products/Images/44/301634/hp-15s-fq2716tu-i3-7c0x3pa-thumb-600x600.jpg',
+        address: 'some address 5, 12345, city',
+        description: 'Some descriptions',
+      },
+    },
+  };
+}
 export default MeetupDetails;
