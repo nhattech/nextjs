b3b8333ff2c8cfa55294ca3182d126aaa9b49917
Duong Ba Nhat <banhatduong@gmailcom>
2023-10-10 11:51:39 +0700

    Working with MongoDB

--- 02-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
+++ 02-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
@@ -1,10 +1,25 @@
-function hanler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function hanler(req, res) {
   const method = req.method;
-  const data = req.body;
-  const { title, image, address, description } = data;
 
   if (method === 'POST') {
-    console.log(method);
+    const data = req.body;
+    const { title, image, address, description } = data;
+
+    const connect = await MongoClient.connect(
+      'mongodb+srv://root:root@cluster0.mvfd5ef.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+
+    const db = connect.db();
+    const meetupCollection = db.collection('meetups');
+    const result = await meetupCollection.insertOne(data);
+
+    console.log(result);
+
+    connect.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
